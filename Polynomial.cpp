6584104cc231f0614804839ed035215d93787356
Alvaro Guerrero Jiménez <alvar@localhost.localdomain>
2019-09-12 17:59:01 -0500

    Everything implemented. Refactoring yet to be done.

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -1,7 +1,181 @@
-//
-// Created by rudri on 9/12/2019.
-//
-
-#include <stdexcept>
+#include <algorithm>
+#include <initializer_list>
+#include <ios>
 #include <iostream>
+#include <utility>
+
 #include "Polynomial.h"
+
+using std::endl;
+using std::initializer_list;
+using std::for_each;
+using std::noshowpos;
+using std::pair;
+using std::showpos;
+
+Polynomial::Polynomial()
+{
+}
+
+Polynomial::Polynomial(Polynomial const& other)
+    : m_polynomial(other.m_polynomial)
+{
+}
+
+Polynomial::Polynomial(initializer_list<pair<coefficient_type, exponent_type>> il)
+{
+    for (auto const& p : il)
+    {
+        if (p.first != coefficient_type())
+        {
+            m_polynomial[p.second] = p.first;
+        }
+    }
+}
+
+Polynomial::~Polynomial()
+{
+}
+
+Polynomial::exponent_type Polynomial::degree() const
+{
+    return m_polynomial.rbegin()->first;
+}
+
+Polynomial& Polynomial::operator+=(Polynomial const& other)
+{
+    for (auto const& ecp : other.m_polynomial)
+    {
+        m_polynomial[ecp.first] += ecp.second;
+    }
+
+    // for_each(other.m_polynomial.cbegin(), other.m_polynomial.cend(),
+    //          [m_polynomial](pair<const exponent_type, coefficient_type> ecp)
+    //          { m_polynomial[ecp.first] = ecp.second; });
+
+    return *this;
+}
+
+Polynomial Polynomial::operator+(Polynomial const& other) const
+{
+    Polynomial ret(*this);
+    ret += other;
+    return ret;
+}
+
+Polynomial& Polynomial::operator+=(coefficient_type const& zero_coef)
+{
+    m_polynomial[exponent_type()] += zero_coef;
+
+    return *this;
+}
+
+Polynomial Polynomial::operator+(coefficient_type const& zero_coef) const
+{
+    Polynomial ret(*this);
+
+    ret += zero_coef;
+
+    return ret;
+}
+
+Polynomial operator+(Polynomial::coefficient_type const& zero_coef, Polynomial const& p)
+{
+    return p + zero_coef;
+}
+
+Polynomial& Polynomial::operator*=(Polynomial const& other)
+{
+    m_polynomial = (*this * other).m_polynomial;
+
+    return *this;
+}
+
+Polynomial Polynomial::operator*(Polynomial const& other) const
+{
+    Polynomial ret;
+
+    for (auto const& ecp1 : m_polynomial)
+    {
+        for (auto const& ecp2 : other.m_polynomial)
+        {
+            ret.m_polynomial[ecp1.first + ecp2.first] += ecp1.second * ecp2.second;
+        }
+    }
+    ret.clear_zeroes();
+
+    return ret;
+}
+
+Polynomial Polynomial::operator*(Polynomial::coefficient_type const& ep) const
+{
+    Polynomial ret;
+
+    for (auto const& ecp : m_polynomial)
+    {
+        ret.m_polynomial[ecp.first] = ecp.second * ep;
+    }
+
+    return ret;
+}
+
+Polynomial operator*(Polynomial::coefficient_type const& ep, Polynomial const& p)
+{
+    return p * ep;
+}
+
+Polynomial Polynomial::operator^(unsigned exp)
+{
+    Polynomial ret({{1, 0}});
+
+    while (exp != 0)
+    {
+        ret *= *this;
+
+        exp--;
+    }
+
+    return ret;
+}
+
+std::ostream& operator<<(std::ostream& os, Polynomial const& p)
+{
+    bool show_sign = false;
+    for (auto const& ecp : p.m_polynomial)
+    {
+        if (show_sign)
+        {
+            os << showpos << ecp.second << noshowpos;
+        }
+        else
+        {
+            os << ecp.second;
+        }
+        show_sign = true;
+        
+        if (ecp.second != Polynomial::coefficient_type())
+        {
+            if (ecp.first != Polynomial::exponent_type())
+            {
+                os << "x^" << ecp.first;
+            }
+        }
+    }
+
+    return os;
+}
+
+void Polynomial::clear_zeroes()
+{
+    auto it = m_polynomial.begin();
+
+    while (it != m_polynomial.end())
+    {
+        if (it->second == coefficient_type())
+        {
+            it = m_polynomial.erase(it);
+        }
+
+        it++;
+    }
+}
--- Polynomial.h
+++ Polynomial.h
@@ -1,15 +1,48 @@
-//
-// Created by ruben on 9/12/2019.
-//
+#ifndef GUARD_POLYNOMIAL_H
+#define GUARD_POLYNOMIAL_H
 
-#ifndef POLINOMIO_POLINOMIO_H
-#define POLINOMIO_POLINOMIO_H
+#include <initializer_list>
+#include <iostream>
+#include <map>
+#include <ostream>
+#include <stdexcept>
+#include <utility>
 
-#include <vector>
-using namespace std;
+class Polynomial
+{
+public:
+    using exponent_type = unsigned;
+    using coefficient_type = int;
 
-class Polynomial {
+private:
+    std::map<exponent_type, coefficient_type> m_polynomial;
 
+public:
+    Polynomial();
+    Polynomial(Polynomial const& other);
+    Polynomial(std::initializer_list<std::pair<coefficient_type, exponent_type>>);
+    ~Polynomial();
+
+    exponent_type degree() const;
+
+    Polynomial& operator+=(Polynomial const& other);
+    Polynomial operator+(Polynomial const& other) const;
+
+    Polynomial& operator+=(coefficient_type const& zero_coef);
+    Polynomial operator+(coefficient_type const& zero_coef) const;
+    friend Polynomial operator+(coefficient_type const& zero_coef, Polynomial const& p);
+
+    Polynomial& operator*=(Polynomial const& other);
+    Polynomial operator*(Polynomial const& other) const;
+    Polynomial operator*(coefficient_type const& ep) const;
+    friend Polynomial operator*(coefficient_type const& ep, Polynomial const& p);
+
+    Polynomial operator^(unsigned exp);
+
+    friend std::ostream& operator<<(std::ostream& os, Polynomial const& p);
+
+private:
+    void clear_zeroes();
 };
 
-#endif //POLINOMIO_POLINOMIO_H
+#endif
